bf6c94f7d792cd15db72de8d5bb05c81d3f7a150
NicolaPicciafuoco <nicola.picciafuoco@gmail.com>
2024-05-14 17:29:41 +0200

    add index page

--- app.py
+++ app.py
@@ -1,41 +1,48 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
 
 app = Flask(__name__)
 
+#route for page of index
 @app.route('/')
 def index():
+    return render_template('index.html')
+
+#route for page of login
+@app.route('/login')
+def login_page():
     return render_template('login.html')
 
 @app.route('/login', methods=['POST'])
 def login():
     try:
-        # Ricevi i dati dalla richiesta POST
+        #
         username = request.form['username']
         password = request.form['password']
 
-        # Connessione al database SQLite
+        # Connessione to database SQLite
         conn = sqlite3.connect('mortenera.sqlite')
         cursor = conn.cursor()
 
-        # Costruisci la query con la concatenazione diretta dei valori dell'utente
+        # build query
         query = "SELECT * FROM users WHERE username = '" + username + "' AND password = '" + password + "'"
         print(query)
-        # Esegui la query
+        # execute  query
         cursor.execute(query)
         user = cursor.fetchone()
 
-        # Chiudi la connessione al database
+        # Close connection
         conn.close()
 
-        # Se l'autenticazione ha successo, restituisci il messaggio di login riuscito
+        # Check if user exists
         if user:
-            return render_template('index.html')
+            return render_template('area_riservata.html')
         else:
             return 'Credenziali non valide.'
     except Exception as e:
         print(e)
         return 'Errore durante il login.'
+
 
 
 if __name__ == '__main__':
