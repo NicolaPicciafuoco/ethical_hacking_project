efd428a87722fa49762cc8253cf122ebd0b59993
FrancescoParisi02 <francescoparisi178@gmail.com>
2024-05-15 11:32:14 +0200

    Add session management

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
+import secrets
 import sqlite3
 
+# Session key generated randomly
+secrets_key = secrets.token_hex(16)
 app = Flask(__name__)
+app.secret_key = secrets_key
 
 #route for page of index
 @app.route('/')
@@ -36,14 +40,22 @@ def login():
 
         # Check if user exists
         if user:
-            return render_template('area_riservata.html')
+            session['username'] = username
+            return redirect(url_for('area_riservata', user=username))
         else:
             return 'Credenziali non valide.'
     except Exception as e:
         print(e)
         return 'Errore durante il login.'
 
-
+@app.route('/area_riservata')
+def area_riservata():
+    # Ottieni il parametro user dall'URL
+    user = session.get('username')
+    if user:
+        return render_template('area_riservata.html', user=user)
+    else:
+        return 'Accesso negato.'
 
 if __name__ == '__main__':
     app.run(debug=True)
